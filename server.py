9ffa128310bf6614c8f90ba0a1c57ac53ff989f2
Elaine Ou <elaine.ou@gmail.com>
2016-02-27 17:54:56 -0800

    update server funcs

--- server.py
+++ server.py
@@ -22,26 +22,42 @@ wallet = Wallet()
 payment = Payment(app, wallet)
 
 PRICE_PER_BEAN = 20000
+SECONDS_PER_BEAN = 2
+
+total_sold = 0
     
 def get_price_from_request(request):
     qty = request.args.get('qty')
     return PRICE_PER_BEAN*qty
 
-# Open GPIO 6 with output direction
-gpio_out = GPIO(6, "out")
-
-gpio_out.write(1)
-
-gpio_out.close()
-
 # buy jellybeans. price depends on qty requested
 @app.route('/buy')
 @payment.required(get_price_from_request)
 def purchase():
+
+    qty = request.args.get('qty')
+    total_sold += int(qty)
+    
+    # Open GPIO 6 with output direction
+    gpio_out = GPIO(6, "out")
+
     # dispense some beans
+    gpio_out.write(True)
+
+    # This needs to be calibrated depending 
+    # on dispenser speed. 
+    time.sleep(2*qty)
+
+    # Stop spitting out beans
+    gpio_out.write(False)
+    gpio_out.close()
 
     return "Paid %d. Please collect your jellybeans." % get_price_from_request
 
+# get total number sold
+@app.route('/total')
+def total():
+    return "A total of %d beans have been purchased." % total_sold
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
