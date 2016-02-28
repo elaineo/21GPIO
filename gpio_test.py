46e90b66820f26b10008a65e433fab330e8ee0f2
Elaine Ou <elaine.ou@gmail.com>
2016-02-27 17:35:56 -0800

    timeout for test

--- gpio_test.py
+++ gpio_test.py
@@ -1,11 +1,18 @@
-## GPIO test
+## GPIO test for Raspberry Pi
 
 # import pin controller
 from periphery import GPIO
+
+import time
 
 # Open GPIO 6 with output direction
 gpio_out = GPIO(6, "out")
 
 gpio_out.write(True)
+
+# sleep for 5 seconds
+time.sleep(5)
+
+gpio_out.write(False)
 
 gpio_out.close()
